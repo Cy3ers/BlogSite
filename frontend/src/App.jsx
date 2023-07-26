52f82da0a906583ce00f55804c87c6646250231b
Cy3ers <faizaan.talha@gmail.com>
2023-07-26 15:05:04 +0500

    Replace js to jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import BlogList from './components/BlogList';
-import Search from './components/Search';
-import TaggedBlogs from './components/TaggedBlogs';
+import Home from './components/Home.jsx';
+import BlogList from './components/BlogList.jsx';
+import Search from './components/Search.jsx';
+import TaggedBlogs from './components/TaggedBlogs.jsx';
 import './styles/App.css';
 
 function App() {
